aab340820a94cba124e5ca30d0080aba32356787
SnehalJadhav <sjadhav@carnival.com>
2023-05-25 23:30:12 +0530

    Initial commit

---
+++ SwiftUI ToDo List/SwiftUI_ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ToDo_ListApp.swift
+//  SwiftUI ToDo List
+//
+//  Created by mnameit on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
